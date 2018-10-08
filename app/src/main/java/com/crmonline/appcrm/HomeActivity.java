fbf987488871f972f900fd98f04c2bfcd98538bb
Rickson <42771773+goocombat@users.noreply.github.com>
2018-10-08 13:16:50 -0300

    Recyler
    ééé

---
+++ app/src/main/java/com/crmonline/appcrm/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.crmonline.appcrm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class HomeActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
