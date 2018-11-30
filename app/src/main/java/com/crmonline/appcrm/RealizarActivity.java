8aeca8f1088cded1852d415ece21aadba6ddca83
Rickson <42771773+goocombat@users.noreply.github.com>
2018-11-30 11:21:59 -0200

    Design Realizar
    hhhh

--- app/src/main/java/com/crmonline/appcrm/RealizarActivity.java
+++ app/src/main/java/com/crmonline/appcrm/RealizarActivity.java
@@ -1,22 +1,30 @@
 package com.crmonline.appcrm;
 
+import android.app.SearchManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 import com.crmonline.appcrm.Entities.VisitaRealizada;
 
 public class RealizarActivity extends AppCompatActivity {
 
+    RatingBar ratingBar;
+    EditText obs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realizar);
 
-        Integer id =  getIntent().getIntExtra("id",0);
+        Integer id = getIntent().getIntExtra("id", 0);
 
-        Log.i("teste",id.toString());
+        Log.i("teste", id.toString());
 
         Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show();
 
@@ -24,5 +32,8 @@ public class RealizarActivity extends AppCompatActivity {
         visitaRealizada.setId(id);
 
 
+
+        
     }
-}
+
+    }
--- app/src/main/java/com/crmonline/appcrm/Services/AgendaService.java
+++ app/src/main/java/com/crmonline/appcrm/Services/AgendaService.java
@@ -1,10 +1,13 @@
 package com.crmonline.appcrm.Services;
 
+import com.crmonline.appcrm.Entities.RespostaGenerica;
 import com.crmonline.appcrm.Entities.Visita;
+import com.crmonline.appcrm.Entities.VisitaRealizada;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
@@ -12,6 +15,10 @@ public interface AgendaService {
 
     @GET("visita/agendadas")
     Call<List<Visita>> buscaVisitas();
+
+    @POST("visita/realizar")
+    Call<RespostaGenerica>visitaRealizada(@Body VisitaRealizada vr);
+
 
 
 
